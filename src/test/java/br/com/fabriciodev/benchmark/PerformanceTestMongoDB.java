2fc7fde32f4adeacf4f13635539ee62abb224707
Fabricio Colombo <fabricio.colombo.mva@gmail.com>
2012-09-17 18:45:45 -0300

    Add tests with mongodb-async-driver.

--- src/test/java/br/com/fabriciodev/benchmark/PerformanceTestMongoDB.java
+++ src/test/java/br/com/fabriciodev/benchmark/PerformanceTestMongoDB.java
@@ -3,9 +3,16 @@ package br.com.fabriciodev.benchmark;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
 
+import com.allanbank.mongodb.MongoCollection;
+import com.allanbank.mongodb.MongoDatabase;
+import com.allanbank.mongodb.MongoFactory;
+import com.allanbank.mongodb.bson.Document;
+import com.allanbank.mongodb.bson.DocumentAssignable;
+import com.allanbank.mongodb.bson.builder.BuilderFactory;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -53,10 +60,50 @@ public class PerformanceTestMongoDB extends BasePerformanceTest {
 			dbObject.put("name", person.getName());
 			dbObject.put("birthDate", person.getBirthDate());
 			dbObject.put("salary", person.getSalary());
-			
+
 			list.add(dbObject);
 		}
 
 		collection.insert(list);
+	}
+
+	@Test
+	public void mongodbAsync() throws InterruptedException, ExecutionException {
+		com.allanbank.mongodb.Mongo mongo = MongoFactory.create("mongodb://localhost:27017/db?maxConnectionCount=10");
+
+		MongoDatabase db = mongo.getDatabase("bench");
+
+		MongoCollection collection = db.getCollection("person");
+
+		collection.drop();
+
+		for (Person person : getPersons()) {
+			Document document = BuilderFactory.start().add("id", person.getId()).add("name", person.getName())
+					.add("birthDate", person.getBirthDate()).add("salary", person.getSalary()).build();
+
+			collection.insertAsync(document);
+		}
+	}
+
+	@Test
+	public void mongodbBatchAsync() {
+		com.allanbank.mongodb.Mongo mongo = MongoFactory.create("mongodb://localhost:27017/db?maxConnectionCount=10");
+
+		MongoDatabase db = mongo.getDatabase("bench");
+
+		MongoCollection collection = db.getCollection("person");
+
+		collection.drop();
+
+		List<Document> list = new ArrayList<Document>();
+
+		for (Person person : getPersons()) {
+			Document document = BuilderFactory.start().add("id", person.getId()).add("name", person.getName())
+					.add("birthDate", person.getBirthDate()).add("salary", person.getSalary()).build();
+
+			list.add(document);
+		}
+
+		collection.insertAsync(list.toArray(new DocumentAssignable[] {}));
 	}
 }
